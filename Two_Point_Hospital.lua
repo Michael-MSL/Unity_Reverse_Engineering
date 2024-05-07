52ff3e682248f88bb44edca30835b3d881dbc69a
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-06 23:29:05 -0500

    Update Two_Point_Hospital.lua

--- Two_Point_Hospital.lua
+++ Two_Point_Hospital.lua
@@ -1,1 +1,5 @@
+CharModuleAssets__.cctor	seg000	00000000001C0990	00000008			R	.	.	.	.	.	.	.
 
+CharModuleAssets__.ctor	seg000	00000000001C08B0	0000002E			R	.	.	.	.	.	.	.
+
+CharacterHappinessComponent__.ctor	seg000	0000000000000C90	0000001F			R	.	.	.	.	.	.	.
