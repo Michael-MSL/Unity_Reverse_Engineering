4f1b06d6e14cd1b724221a5d0de203e4755c201c
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-06 23:29:16 -0500

    Update Two_Point_Hospital.lua

--- Two_Point_Hospital.lua
+++ Two_Point_Hospital.lua
@@ -3,3 +3,9 @@ CharModuleAssets__.cctor	seg000	00000000001C0990	00000008			R	.	.	.	.	.	.	.
 CharModuleAssets__.ctor	seg000	00000000001C08B0	0000002E			R	.	.	.	.	.	.	.
 
 CharacterHappinessComponent__.ctor	seg000	0000000000000C90	0000001F			R	.	.	.	.	.	.	.
+
+ChildModuleChoice__.ctor	seg000	00000000001C0A00	00000012			R	.	.	.	.	.	.	.
+
+Clip__.ctor	seg000	00000000001BE140	0000001D			R	.	.	.	.	.	.	.
+
+ConfigData__.ctor	seg000	00000000001D15F0	0000004C			R	.	.	.	.	.	.	.
