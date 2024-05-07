dd48c4e51577afdf04ff52cd5d5b76b2dd7741b5
Michael-MSL <michael.schneider_uh@proton.me>
2024-05-06 23:29:26 -0500

    Update Two_Point_Hospital.lua

--- Two_Point_Hospital.lua
+++ Two_Point_Hospital.lua
@@ -9,3 +9,9 @@ ChildModuleChoice__.ctor	seg000	00000000001C0A00	00000012			R	.	.	.	.	.	.	.
 Clip__.ctor	seg000	00000000001BE140	0000001D			R	.	.	.	.	.	.	.
 
 ConfigData__.ctor	seg000	00000000001D15F0	0000004C			R	.	.	.	.	.	.	.
+
+ConfigData__.ctor_0	seg000	00000000001D7D80	00000053			R	.	.	.	.	.	.	.
+
+Config__.ctor	seg000	00000000001BBCA0	000002A8			R	.	.	.	.	.	.	.
+
+Config__.ctor_1	seg000	00000000001BF970	00000028			R	.	.	.	.	.	.	.
