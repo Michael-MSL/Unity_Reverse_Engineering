536172d06e56c7fe1d51e56ea7880f53864cf704
Michael <michael.schneider_uh@proton.me>
2024-05-08 00:01:45 -0500

    Update Thief_Simulator_2.lua

--- Thief_Simulator_2.lua
+++ Thief_Simulator_2.lua
@@ -1,1 +1,5 @@
+AGGlobalSettings__.ctor	seg000	0000000000054A40	00000045			R	.	.	.	.	.	.	.
 
+AGGlobalSnowProps__.ctor	seg000	0000000000054E60	00000028			R	.	.	.	.	.	.	.
+
+AGGlobalSnow__.ctor	seg000	0000000000054C10	0000008E			R	.	.	.	.	.	.	.
