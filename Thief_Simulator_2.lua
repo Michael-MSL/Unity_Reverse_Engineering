ac24041198eeaf5dab6119db07b2dbb8f2ee99dd
Michael <michael.schneider_uh@proton.me>
2024-05-08 00:02:06 -0500

    Update Thief_Simulator_2.lua

--- Thief_Simulator_2.lua
+++ Thief_Simulator_2.lua
@@ -9,3 +9,9 @@ AGGlobalWind__.ctor	seg000	0000000000055070	00000066			R	.	.	.	.	.	.	.
 AI__.ctor	seg000	00000000000733D0	00000186			R	.	.	.	.	.	.	.
 
 AI__AllBoolsFalse	seg000	000000000006E710	000000CA			R	.	.	.	.	.	.	.
+
+AI__ApplyEvent	seg000	000000000006D660	0000041F			R	.	.	.	.	.	.	.
+
+AI__CheckMood	seg000	000000000006F670	0000015C			R	.	.	.	.	.	.	.
+
+AI__Discover_Breakin	seg000	000000000006EC70	000000B2			R	.	.	.	.	.	.	.
