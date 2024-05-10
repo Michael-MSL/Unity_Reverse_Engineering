c04668b459e2f5bb42ab691320eb6c72b68fc401
Michael <michael.schneider_uh@proton.me>
2024-05-10 01:01:53 -0500

    Update Contraband_Police.lua

--- Contraband_Police.lua
+++ Contraband_Police.lua
@@ -5,3 +5,11 @@ AIControllerData__.ctor	seg000	0000000000254530	0000001F			R	.	.	.	.	.	.	.
 AIManager__.ctor	seg000	0000000000097740	00000015			R	.	.	.	.	.	.	.
 
 AIManager__Init	seg000	0000000000095950	0000011A			R	.	.	.	.	.	.	.
+
+AIManager__InitMasks	seg000	0000000000096080	0000007F			R	.	.	.	.	.	.	.
+
+AIPool__.ctor	seg000	0000000000061DE0	00000049			R	.	.	.	.	.	.	.
+
+AITarget__.ctor	seg000	000000000002B4F0	0000002B			R	.	.	.	.	.	.	.
+
+ActionPointLimits__.ctor	seg000	0000000000070E80	0000003E			R	.	.	.	.	.	.	.
