f0af8377082bf7f9524017725c9c3f2a77f2f211
Michael <michael.schneider_uh@proton.me>
2024-05-10 01:02:04 -0500

    Update Contraband_Police.lua

--- Contraband_Police.lua
+++ Contraband_Police.lua
@@ -13,3 +13,11 @@ AIPool__.ctor	seg000	0000000000061DE0	00000049			R	.	.	.	.	.	.	.
 AITarget__.ctor	seg000	000000000002B4F0	0000002B			R	.	.	.	.	.	.	.
 
 ActionPointLimits__.ctor	seg000	0000000000070E80	0000003E			R	.	.	.	.	.	.	.
+
+ActionSpot__.ctor	seg000	00000000000299D0	00000089			R	.	.	.	.	.	.	.
+
+Alcmeter__TurnOff	seg000	0000000000086D30	00000047			R	.	.	.	.	.	.	.
+
+AltitudePortalRespawner__.ctor	seg000	00000000000A2F40	00000012			R	.	.	.	.	.	.	.
+
+AltitudePortalRespawner__Awake	seg000	00000000000A2CF0	0000000C			R	.	.	.	.	.	.	.
