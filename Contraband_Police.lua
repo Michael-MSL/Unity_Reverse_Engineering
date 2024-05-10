bcb945a78db53d1f330f4a167012b21e702364a8
Michael <michael.schneider_uh@proton.me>
2024-05-10 01:01:42 -0500

    Update Contraband_Police.lua

--- Contraband_Police.lua
+++ Contraband_Police.lua
@@ -1,1 +1,7 @@
+AICharacterMovement__.ctor	seg000	0000000000029F60	0000002C			R	.	.	.	.	.	.	.
 
+AIControllerData__.ctor	seg000	0000000000254530	0000001F			R	.	.	.	.	.	.	.
+
+AIManager__.ctor	seg000	0000000000097740	00000015			R	.	.	.	.	.	.	.
+
+AIManager__Init	seg000	0000000000095950	0000011A			R	.	.	.	.	.	.	.
