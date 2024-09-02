59d18b37de1f61fd526e85c58871b8189d4c2599
Michael <michael.schneider_uh@proton.me>
2024-09-01 20:41:18 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -363,3 +363,9 @@ DevConsole__Vodka	seg000	00000000000F1830	00000025			R	.	.	.	.	.	.	.
 DevConsole__Warning	seg000	00000000000F1B20	0000000E			R	.	.	.	.	.	.	.
 
 DevConsole__WeakGun	seg000	00000000000ED780	0000007D			R	.	.	.	.	.	.	.
+
+DocumentsDataGenerator__.cctor	seg000	0000000000049810	000003B5			R	.	.	.	.	.	.	.
+
+DocumentsData__AddVehicleDocs	seg000	000000000004C710	00000229			R	.	.	.	.	.	.	.
+
+DoorSounds__.ctor	seg000	00000000002BEA20	00000024			R	.	.	.	.	.	.	.
