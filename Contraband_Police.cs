be4e278d44a0e77b8389193b57371f9689d84748
Michael <michael.schneider_uh@proton.me>
2024-09-01 20:44:47 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -423,3 +423,9 @@ GameDataSave__DeleteGameSaves	seg000	000000000009BC80	00000028			R	.	.	.	.	.	.	.
 GameDataSave__GetFilePathToSave	seg000	000000000009BB70	00000058			R	.	.	.	.	.	.	.
 
 GameDataSave__GetSavePrefix	seg000	000000000009B9D0	0000002D			R	.	.	.	.	.	.	.
+
+GameDataSave__GetSavedFilePath	seg000	000000000009BA20	0000004C			R	.	.	.	.	.	.	.
+
+GameDataSave__GetSlotSaveDirectory	seg000	000000000009B9B0	00000020			R	.	.	.	.	.	.	.
+
+GameDataSave__GetSlotsPaths	seg000	000000000009BB10	00000051			R	.	.	.	.	.	.	.
