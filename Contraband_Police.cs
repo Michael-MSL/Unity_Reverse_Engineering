fabc579bdf07d9cdfe273c074e5c788099e28039
Michael <michael.schneider_uh@proton.me>
2024-09-01 20:46:41 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -459,3 +459,9 @@ GameMessagesCreator__ContrabandFound	seg000	00000000000E2DE0	00000067			R	.	.	.	
 GameMessagesCreator__ContrabandItemFound	seg000	00000000000E2F50	00000059			R	.	.	.	.	.	.	.
 
 GameMessagesCreator__ContrabandTransferred	seg000	00000000000E3380	0000007A			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__CorrectArrest	seg000	00000000000E2CF0	00000062			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__DailyMaintenance	seg000	00000000000E3100	000000C4			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__DailyMaintenanceSurvival	seg000	00000000000E31D0	00000072			R	.	.	.	.	.	.	.
