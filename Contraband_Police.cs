c6dd9903b92519c24315c9f33f90fc5aa5c9413e
Michael <michael.schneider_uh@proton.me>
2024-09-01 20:47:22 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -471,3 +471,9 @@ GameMessagesCreator__DailyUpkeep	seg000	00000000000E2C20	00000063			R	.	.	.	.	.	
 GameMessagesCreator__EndOfWork	seg000	00000000000E3AB0	0000005A			R	.	.	.	.	.	.	.
 
 GameMessagesCreator__FatalAccident	seg000	00000000000E2A80	00000077			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__HeavyWounded	seg000	00000000000E27B0	00000070			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__InflationIncreased	seg000	00000000000E3BE0	0000005B			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__ItemsAfterIntro	seg000	00000000000E3710	0000005B			R	.	.	.	.	.	.	.
