b4337b8035fec9322185efe3088565cf46ad8861
Michael <michael.schneider_uh@proton.me>
2024-09-01 20:41:35 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -369,3 +369,9 @@ DocumentsDataGenerator__.cctor	seg000	0000000000049810	000003B5			R	.	.	.	.	.	.	
 DocumentsData__AddVehicleDocs	seg000	000000000004C710	00000229			R	.	.	.	.	.	.	.
 
 DoorSounds__.ctor	seg000	00000000002BEA20	00000024			R	.	.	.	.	.	.	.
+
+DoorTexts__.ctor	seg000	00000000002BEA60	00000028			R	.	.	.	.	.	.	.
+
+DrinkableVodka__.ctor	seg000	000000000007CBE0	00000012			R	.	.	.	.	.	.	.
+
+DrinkableVodka__ActionSingle	seg000	000000000007CB20	00000055			R	.	.	.	.	.	.	.
