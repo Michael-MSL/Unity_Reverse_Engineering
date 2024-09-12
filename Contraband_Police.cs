fe0b595b437bcd771823ddb0ed9f55e41bd448cc
Michael <michael.schneider_uh@proton.me>
2024-09-12 00:16:22 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -709,3 +709,9 @@ IntroDay1__OnInspectionReportShown	seg000	0000000000075DA0	0000007E			R	.	.	.	.	
 IntroDay1__OnPlayerApproachedPost	seg000	00000000000755E0	0000006E			R	.	.	.	.	.	.	.
 
 IntroDay1__OptionSelected	seg000	0000000000075E20	00000042			R	.	.	.	.	.	.	.
+
+IntroDay1__PlayerEnteredTrailer	seg000	0000000000076040	00000039			R	.	.	.	.	.	.	.
+
+IntroDay1__PlayerEnteredTrailerInterior	seg000	0000000000075FE0	00000057			R	.	.	.	.	.	.	.
+
+IntroDay1__PrepareDay	seg000	0000000000074C40	000000C0			R	.	.	.	.	.	.	.
