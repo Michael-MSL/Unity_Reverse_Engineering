9e638ae9f77ba9a46ce8da89cb49cdf10f264586
Michael <michael.schneider_uh@proton.me>
2024-09-11 21:43:25 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -501,3 +501,9 @@ GameMessagesCreator__PrisonBreak	seg000	00000000000E37C0	0000005A			R	.	.	.	.	.	
 GameMessagesCreator__PrisonBreakVehicle	seg000	00000000000E3820	0000005A			R	.	.	.	.	.	.	.
 
 GameMessagesCreator__PrisonFull	seg000	00000000000E3090	00000062			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__PrisonerTransferred	seg000	00000000000E2B00	0000009F			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__PrisonersTransferred	seg000	00000000000E3300	0000007A			R	.	.	.	.	.	.	.
+
+GameMessagesCreator__PursuitFailedTimeLimit	seg000	00000000000E2FB0	00000066			R	.	.	.	.	.	.	.
