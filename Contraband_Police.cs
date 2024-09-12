25d41905f0e2865ac0b67516cf72fac585c5c48c
Michael <michael.schneider_uh@proton.me>
2024-09-11 21:58:31 -0500

    Update Contraband_Police.cs

--- Contraband_Police.cs
+++ Contraband_Police.cs
@@ -665,3 +665,11 @@ InspectionReportData__CheckDocuments	seg000	0000000000051E50	000002C9			R	.	.	.	
 InspectionZone__GetMaxWeight	seg000	000000000008EF50	00000057			R	.	.	.	.	.	.	.
 
 InspectionZone__Init	seg000	000000000008D6A0	0000004C			R	.	.	.	.	.	.	.
+
+InspectionZone__OnReturnToPostFailed	seg000	000000000008ED00	00000044			R	.	.	.	.	.	.	.
+
+InspectionZone__OnReturnToPostSucceded	seg000	000000000008ED50	00000031			R	.	.	.	.	.	.	.
+
+InspectionZone__OnVehicleApproeachedBarrier	seg000	000000000008DCE0	0000002C			R	.	.	.	.	.	.	.
+
+InspectionZone__PlayHurryUp	seg000	000000000008F770	0000003E			R	.	.	.	.	.	.	.
